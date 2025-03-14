d34c512389464b137ed0cfbf5dfb371c2043bfd1
chibuezemicahe <chibuezemicahe@gmail.com>
2025-03-14 06:58:09 +0100

    fix: axios throwing error and causing server to crash

--- src/agent/errorInterceptor.ts
+++ src/agent/errorInterceptor.ts
@@ -1,40 +1,39 @@
 import { logger } from "../utils/logger";
-
 import axios from "axios";
 import { config } from "../config/config";
 
-const reportError = async (error: Error, type: string) => {
-    if (!config.MICRO_SERVICE_URL) {
-      logger.warn(`Skipping error reporting: MICRO_SERVICE_URL is not set.`);
-      return;
-    }
-  
-    logger.info(`Reporting ${type} to ${config.MICRO_SERVICE_URL}/api/errors`);
-  
-    try {
-      await axios.post(`${config.MICRO_SERVICE_URL}/api/errors`, {
-        type,
-        message: error.message,
-        stack: error.stack,
-        timestamp: new Date().toISOString(),
+const reportError = async (error: Error, type: string): Promise<void> => {
+  if (!config.MICRO_SERVICE_URL) {
+    logger.warn("Skipping error reporting: MICRO_SERVICE_URL is not set.");
+    return;
+  }
+
+  logger.info(`Reporting ${type} to ${config.MICRO_SERVICE_URL}/api/errors`);
+
+  try {
+    await axios.post(`${config.MICRO_SERVICE_URL}/api/errors`, {
+      type,
+      message: error.message,
+      stack: error.stack,
+      timestamp: new Date().toISOString(),
+    });
+
+    logger.info(`Reported ${type} to microservice.`);
+  } catch (err: unknown) {
+
+    if (err instanceof Error) {
+      logger.error(`Failed to report ${type}: ${err.message}`, {
+        stack: err.stack,
       });
-  
-      logger.info(`Reported ${type} to microservice.`);
-    } catch (err) {
-        if (axios.isAxiosError(err)) {
-            logger.error(`Failed to report ${type}: ${err.message}`, {
-              status: err.response?.status,
-              data: err.response?.data,
-              config: err.config,
-            });
-          } else {
-            logger.error(`Failed to report ${type}:`, err);
-          }
+    } else {
+      logger.error(`Failed to report ${type}: Unknown error`, err);
     }
-  };
-  
+
+  }
+};
+
 // Function to set up error interception
-export const setupErrorInterceptor = () => {
+export const setupErrorInterceptor = (): void => {
   process.on("uncaughtException", (error) => {
     logger.error(`Uncaught Exception: ${error.message}`);
     reportError(error, "uncaughtException");
@@ -43,7 +42,7 @@ export const setupErrorInterceptor = () => {
   process.on("unhandledRejection", (error) => {
     if (error instanceof Error) {
       logger.error(`Unhandled Rejection: ${error.message}`);
-      setImmediate(() => reportError(error, "uncaughtException"));
+      setImmediate(() => reportError(error, "unhandledRejection"));
     } else {
       logger.error(`Unhandled Rejection: ${String(error)}`);
     }
