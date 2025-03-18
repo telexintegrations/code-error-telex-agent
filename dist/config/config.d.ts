3265d67a079f8e7f3b79e6dc4fc94b6896ec7315
semilore <semilore@omotade-michaels.com>
2025-03-18 23:02:07 +0100

    feat: package installability

---
+++ dist/config/config.d.ts
@@ -0,0 +1,7 @@
+export interface Config {
+    PORT: number;
+    MICRO_SERVICE_URL: string;
+    CHANNEL_ID: string;
+    LOG_LEVEL: string;
+}
+export declare const config: Config;
---
+++ dist/zeromqService.d.ts
@@ -0,0 +1,3 @@
+import * as zmq from 'zeromq';
+export declare function initializeZeroMqClient(): Promise<zmq.Request>;
+export declare function sendZeroMqMessage(sock: zmq.Request, message: string): Promise<string>;
